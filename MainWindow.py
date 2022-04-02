ed70c71ddf5953eb57f9a01399b94e78cd9c16aa
noshotbigjuicersbilal151 <bnasi004@ucr.edu>
2022-04-02 12:53:43 -0700

    working manual addition, outputs array of sorted assignments

--- MainWindow.py
+++ MainWindow.py
@@ -1,36 +1,65 @@
 from tkinter import *
+import string
+
+global allAssignments
+
+allAssignments  = []
+
 window=Tk()
 
 #set geometry and title of main window
 window.title('Course Scheduler')
 window.geometry("500x400")
 
+
+
+
 #define functions of buttons
+def generateCalendar():
+    global allAssignments
+
+    allAssignments.sort(key = lambda x: int(x[1]))
+    print(allAssignments)
+
 def openManualWindow():
     manualWindow = Toplevel(window)
     manualWindow.title("Manual Assignment Insertion")
     manualWindow.geometry("500x400")
 
-    lbl=Label(manualWindow, text="Assignment Name")
-    lbl.place(x=60, y=50)
-    txtfld=Entry(manualWindow, bd=5)
-    txtfld.place(x=60, y=80)
+    lbl=Label(manualWindow, text="Assignment Name").grid(row = 1, sticky = W)
+    AName=Entry(manualWindow)
 
-    btn = Button(manualWindow, text="Add Assignment")
-    btn.place(x=50, y=400)
+    lbl=Label(manualWindow, text="Assignment Due Date (mm/dd/year)").grid(row = 3, sticky = W)
+    ADueDate=Entry(manualWindow)
 
+    AName.grid(row = 1, column = 1)
+    ADueDate.grid(row = 3, column = 1)
+
+    def getInput():
+        global allAssignments
+        name=AName.get()
+        dueDate=ADueDate.get()
+        #dueDate.translate(None, string.letters)
+        dueDate = dueDate.translate(dueDate.maketrans("", "", string.ascii_letters))
+        manualWindow.destroy()
+        assignmentInfo = [name, dueDate]
+        print("added ", assignmentInfo)
+        allAssignments.append(assignmentInfo)
+
+
+    btn = Button(manualWindow, text="Add Assignment", command = getInput).grid(row = 5, sticky = W)
 
 
 
 
 # add widgets here
-btn = Button(window, text="Manual Assignment Insertion", fg="black", command=openManualWindow)
+btn = Button(window, text="Manually Add Assignment", fg="black", command=openManualWindow)
 btn.place(x=50, y=100)
 
 btn = Button(window, text="Sign in with Canvas", fg="black")
 btn.place(x=300, y=100)
 
-btn = Button(window, text="View Calendar", fg='black')
+btn = Button(window, text="Generate Calendar", fg='black', command = generateCalendar)
 btn.place(x=200, y=250)
 
 window.mainloop()
